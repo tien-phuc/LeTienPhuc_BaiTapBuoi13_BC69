95faf48895d68c9f645d5c8845a168d1dabc524d
tienphuc <tienphuc.vlb@gmail.com>
2024-05-18 11:46:34 +0700

    11h46

---
+++ index.js
@@ -0,0 +1,15 @@
+// Bài 1
+/** mô hình 3 khối
+ * 
+ * đầu vào:
+ * + Lương 1 ngày
+ * + Số ngày làm
+ *
+ * xử lí:
+ * -Công thức: Lương 1 ngày * Số ngày làm
+ * 
+ * đầu ra:
+ * số tiền lương nhân viên dựa trên công thức trên
+ * 
+ * 
+ */
